4c2bad8d6c2fc7c2c2f2afe292d9cec0e01a18db
prince <princekofasiedu@gmail.com>
2023-09-29 08:04:22 +0000

    Update 2023_09_29_080054_create_comments_table.php
    Designed comment table

--- database/migrations/2023_09_29_080054_create_comments_table.php
+++ database/migrations/2023_09_29_080054_create_comments_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->integer('comment_id');
+            $table->integer('user_id');
+            $table->integer('book_id');
+            $table->text('content');
             $table->timestamps();
         });
     }
