07c58a1f18de724ce01fac4dbfa0386a38bae6cd
prince <princekofasiedu@gmail.com>
2023-10-03 17:28:42 +0000

    Update app.blade.php
    Changed divs for background to fixed colors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     </head>
     <body class="">
-        <span class="bg-[white] w-[45vw] h-[100vh] -z-10 absolute border-2 border-solid border-[blue]"></span>
-        <span class="bg-[#f0eee2] w-[55vw] h-[100vh] absolute -z-10 "></span>
+        <span class="bg-[white] w-[45vw] h-[100vh] -z-10 fixed border-2 border-solid border-[blue]"></span>
+        <span class="bg-[#f0eee2] w-[55vw] h-[100vh] fixed -z-10 "></span>
        {{$slot}}
     </body>
 </html>
